5126b402f1c4b57f5ad5b0cffe1f711295aaf687
Kieran <kieran.a.gibb@gmail.com>
2017-11-06 16:12:21 +0000

    Configure redis and active record in their own initializers

---
+++ config/initializers/event_store.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module EventStore
+  class << self
+    def client
+      Rails.application.config.event_store
+    end
+  end
+end
+
+Rails.application.config.event_store.tap do |event_store|
+  event_store.subscribe(Listeners::Members::Purchase, [Events::Member::Purchase])
+end
