aea3b263ee549ec331637c9e08f905c95724b629
Kieran <kieran.a.gibb@gmail.com>
2017-11-07 12:12:57 +0000

    Use coin decorator to present central reserve and coin header

--- app/decorators/coin_decorator.rb
+++ app/decorators/coin_decorator.rb
@@ -3,7 +3,15 @@
 class CoinDecorator < Draper::Decorator
   delegate_all
 
+  def header
+    "#{name} (#{code})"
+  end
+
   def balance
     h.current_member.coin_balance(id) / 10**subdivision
+  end
+
+  def central_reserve_balance
+    h.number_to_currency(central_reserve.to_d / 10**subdivision, precision: subdivision, unit: '')
   end
 end
